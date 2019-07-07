f9e925b714bd1da37d050ef3d4c46198247f4463
buddhalikecat <dakerworm@gmail.com>
2019-07-07 16:45:08 +0800

    Create settings.py

---
+++ linkcollect/linkcollect/settings.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for linkcollect project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'linkcollect'
+
+SPIDER_MODULES = ['linkcollect.spiders']
+NEWSPIDER_MODULE = 'linkcollect.spiders'
+
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 10
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+#设置下裁之后的自动延迟
+DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+# Pipeline的并发数。同时最多可以有多少个Pipeline来处理item
+CONCURRENT_ITEMS = 200
+# 对一个网站的最大并发数
+CONCURRENT_REQUESTS_PER_DOMAIN = 50
+# 对一个IP的最大并发数
+#CONCURRENT_REQUESTS_PER_IP = 50
+
+# Disable cookies (enabled by default)
+COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+SPIDER_MIDDLEWARES = {
+    'linkcollect.middlewares.LinkcollectSpiderMiddleware': 543,
+}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    'linkcollect.middlewares.LinkcollectDownloaderMiddleware': 543,
+}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    'linkcollect.pipelines.LinkcollectPipeline': 300,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#开启访问频率限制
+AUTOTHROTTLE_ENABLED = True
+#设置访问开始的延迟
+AUTOTHROTTLE_START_DELAY = 5
+#设置访问之间的最大延迟
+AUTOTHROTTLE_MAX_DELAY = 60
+#设置Scrapy 并行发给每台远程服务器的请求数量
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
